c59fc6334a9d72dde0e702ce9dc84d26231d944b
joseluiz123 <joseluiz140697@gmail.com>
2020-05-12 10:49:03 -0300

    Add files via upload

---
+++ bootstrap/bootstrap.css
@@ -0,0 +1,82 @@
+
+
+
+
+@charset "utf-8";
+/* CSS Document */
+
+      body 
+		{
+		font-family: Arial Bold;
+		margin:0;
+		padding: 0;
+		text-align: center;
+		
+		min-width:450px;
+		}
+		
+		#menu {
+			font-family:Arial Bold;
+			color:#B33A41;
+			text-transform:uppercase;
+			font-size:40px;
+			width: 100%;
+			position: fixed;
+			top:0;
+			left:0;
+			
+			padding:10px;
+			padding:.5rem;
+			margin:0 0  0px ;
+			}
+		
+		#home { 
+	    height: 300px;
+		width: 100%;
+		padding-top:0px;
+		
+		}
+		
+		#quemsou { 
+	    padding-top:0px;
+	 	height:500px;
+		padding-top:0px;
+		width: 100%;
+		background:#DDAE9E;
+		}
+		
+		#wfisica { 
+		height:300px;
+		width: 100%;
+		padding-top:0px;
+		background-color:#F75258;
+		
+		}
+		#wmath { 
+		height:300px;
+		width: 100%;
+		padding-top:0px;
+		background-color:#DDAE9E;
+		text-color:#000000;
+		
+		
+		}
+
+
+		
+		a:link, a:visited{
+			color:orange;
+			text-decoration:none;
+		}
+		
+		a:hover {
+			color:#FFFF00;
+			text-decoration:none;
+			
+		}
+		
+		#Titulo  {
+			font-size:16px;
+			test-align: center;
+			padding-top: 20px;
+		}
